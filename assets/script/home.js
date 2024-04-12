f7fbe2b9f6b5c4d686d248f4b97693da05ddbe0d
Gabe <G_Linski@hotmail.com>
2024-04-11 23:46:55 -0500

    another update

--- assets/script/home.js
+++ assets/script/home.js
@@ -4,6 +4,33 @@
 
 
 
+async function getUsers(endpoint){
+  const URL = 'https://randomuser.me/api/?nat=CA&results=10&seed=same';
+  const options = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/JSON; charset=UTF-8'
+    },
+    mode: 'cors'
+  }
+  try {
+    const result = await fetch(endpoint,options);
+    if (!result.ok){
+      throw new Error(`${result.statusText} ${result.status}`)
+    }
+    const data = await result.json();
+    console.log(data.results);
+
+  }
+  catch(error){
+    console.log(error.message);
+    
+  }
+
+} 
+getUsers(URL);
+
+
 
 
 
