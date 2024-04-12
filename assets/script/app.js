30ad7ac4a658ca8e5a9af503f5f1e2847d5c839c
Gabe <G_Linski@hotmail.com>
2024-04-11 23:14:05 -0500

    hopefull update

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,10 +1,42 @@
-import classes from './class.js';
-import * as utils from './utils.js';
+
+const login = document.querySelector('.login-button');
+const emailInput = document.getElementById('email-input');
+const passwordInput = document.getElementById('password-input');
+
+const acccess = {
+  "password": "1234",
+  "email": "andre@fakemail.com"
+}
+
+localStorage.setItem('credentials', JSON.stringify(acccess));
+
+//Return LocalStorage
+function getLocalStorage(){
+  return localStorage.getItem('credentials');
+}
+
+
+//Get Values from Input
+function getFormData(){
+  passwordEntered = passwordInput.value;
+  emailEntered = emailInput.value;
+  
+  let credentials = JSON.parse(getLocalStorage());
+  let password = credentials.password;
+  let email = credentials.email;
+
+  if(password === passwordEntered && email === emailEntered.toString()){
+    window.location.href = "./home.html";
+  }
+  else{
+    console.log('not here');
+  }
+}
 
 
 
 
-
+login.addEventListener('click', getFormData);
 
 
 
